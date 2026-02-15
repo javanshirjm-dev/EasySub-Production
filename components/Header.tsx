a4a70c68f4f6cc2d437851b0986f17d1fe3dc246
JavanshirM <javanshirjm@code.edu.az>
2026-02-16 01:52:23 +0400

    fgvbhjn

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-blue-50/80 backdrop-blur-md sticky top-0 z-50 border-b border-blue-100 shadow-sm">
+        <header className="bg-blue-50 backdrop-blur-md sticky top-0 z-50 border-b border-blue-100 shadow-sm">
             <div className="max-w-7xl mx-auto px-4 h-20 flex items-center justify-between gap-4">
 
                 {/* --- LOGO --- */}
